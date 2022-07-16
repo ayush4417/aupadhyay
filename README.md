54a45f544342c7038eb043b1087f6d6841e004b7
ayush4417 <64230950+ayush4417@users.noreply.github.com>
2022-07-16 14:52:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# aupadhyay
-portfolio 
+# pbehre.in website root
+[![Node.js CI](https://github.com/pavitra14/pbehrein/actions/workflows/node.js.yml/badge.svg?branch=main)](https://github.com/pavitra14/pbehrein/actions/workflows/node.js.yml)
+
+Built on Next.js
---
+++ next.config.js
@@ -0,0 +1,38 @@
+const { getPath } = require("./scripts/getExportPathMap");
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+module.exports = (phase) => {
+  var props = {
+    distDir: "build",
+    images: {
+      loader: "imgix",
+      path: "https://example.com/myaccount/",
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require("./scripts/sitemap");
+      }
+
+      return config;
+    },
+    env: {
+      mode: "",
+    },
+  };
+
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  // when `next build` or `npm run build` is used
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+
+  if (isDev) {
+    props.env.mode = "DEV";
+  }
+  if (isProd) {
+    props.env.mode = "PROD";
+  }
+  return props;
+};
